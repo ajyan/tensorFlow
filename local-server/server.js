c84cbfd18898df3b74bca36789c9e9cd3902fa9e
Andrew Jyan <andrew.jyan4@gmail.com>
2020-06-17 15:14:55 -0400

    first commit

---
+++ local-server/server.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.listen(3001, () => {
+  console.log('app listening on 3001');
+});
